62c3b4daa5535d78abecf5285c313ea67fd4dd74
heros20 <heros40@hotmail.fr>
2021-03-29 19:20:30 +0200

    page menu

--- wp-content/themes/blank_1/template_menu.php
+++ wp-content/themes/blank_1/template_menu.php
@@ -1,11 +1,11 @@
 <?php
-/* Template Name: carte */
+/* Template Name: menu */
 
 get_header();
 ?>
 <div class="B">
     <h1>BIENVENUE</h1>
-    <p>Sur la page carte</p>
+    <p>Sur la page menu</p>
 </div>
 
 <?php
