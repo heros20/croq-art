64dbca2732f354f6f109cb4909a05b578215cacb
heros20 <heros40@hotmail.fr>
2021-03-29 11:35:24 +0200

    add wp

---
+++ wp-content/themes/blank_1/footer.php
@@ -0,0 +1,11 @@
+<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>ceci est le footer</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
