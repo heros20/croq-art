d4a442e26eab4b886745f5a63937a62b33c22859
ZenonStella <stellazenon@gmail.com>
2021-03-31 12:49:41 +0200

    css menu page

--- wp-content/themes/blank_1/template_menu.php
+++ wp-content/themes/blank_1/template_menu.php
@@ -4,33 +4,34 @@
 get_header();
 ?>
 <div class="B">
-    <h1>BIENVENUE</h1>
-    <p>Sur la page menu</p>
+    <div class="menu">
+        <div class="blanc">
+            <div class="marge_grullo">
+                <div class="logo">
+                    <img src="<?= get_template_directory_uri(); ?>/assets/images/Plan_de_travail.png" alt="">
+                </div>  
+            </div>
+            
+        </div>
+        <div class="grullo">
+            <div class="marge_blanc">
+
+            </div>
+        </div>
+        <div class="grullo">
+            <div class="marge_blanc">
+
+            </div>
+        </div>
+        <div class="blanc">
+            <div class="marge_grullo">
+            
+            </div>
+            
+        </div>
+    </div>
 </div>
 
-<div class="menu">
-    <div class="blanc">
-        <div class="mage_grullo">
-            <div class="logo">
-                <img src="<?= get_template_directory_uri(); ?>/assets/images/Plan_de_travail.png" alt="">
-            </div>  
-        </div>
-        
-    </div>
-    <div class="grullo">
-
-    </div>
-    <div class="grullo">
-
-    </div>
-    <div class="blanc">
-        <div class="mage_grullo">
-           
-        </div>
-        
-    </div>
-
-</div>
 
 <?php
 get_footer();
