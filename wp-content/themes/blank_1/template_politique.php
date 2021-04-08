f2878b867110e77fd530a590c7e3b307a02be2c5
heros20 <heros40@hotmail.fr>
2021-04-08 11:44:50 +0200

    page de footer end

---
+++ wp-content/themes/blank_1/template_politique.php
@@ -0,0 +1,154 @@
+<?php
+/* Template Name: politique  */
+
+
+get_header();
+?>
+
+
+<div class="mentions wrap2">
+              
+                <h2>Politique de confidentialité</h2>
+<p>Dernière mise à jour : 08 avril 2021</p>
+<p>La présente Politique de confidentialité décrit Nos politiques et procédures en matière de collecte, d'utilisation et de divulgation de Vos informations lorsque Vous utilisez le Service et vous informe de Vos droits en matière de confidentialité et de la manière dont la loi vous protège.</p> <p>.
+<p>Nous utilisons vos données personnelles pour fournir et améliorer le Service. En utilisant le Service, vous acceptez la collecte et l'utilisation des informations conformément à la présente Politique de confidentialité. Cette politique de confidentialité a été créée à l'aide du <a href="https://www.privacypolicies.com/privacy-policy-generator/" target="_blank">Générateur de politique de confidentialité</a>.</p> <p>.
+<h1>Interprétation et définitions</h1>
+<h2>Interprétation</h2>
+<p>Les mots dont la lettre initiale est en majuscule ont des significations définies dans les conditions suivantes. Les définitions suivantes ont le même sens, qu'elles apparaissent au singulier ou au pluriel.</p>
+<h2>Définitions</h2>
+<p>Pour les besoins de la présente politique de confidentialité :</p>
+<ul>
+<li>
+<p><strong>Compte</strong> désigne un compte unique créé pour vous afin d'accéder à notre service ou à certaines parties de notre service.</p>
+</li>
+<li>
+</p><strong>Société</strong> (désignée soit par &quot;la Société&quot ;, &quot;Nous&quot ;, &quot;Nous&quot ; ou &quot;Notre&quot ; dans le présent Accord) désigne croq'artCafé.</p> <p>.
+</li>
+<li>
+</p><strong>Les cookies</strong> sont de petits fichiers placés sur votre ordinateur, votre appareil mobile ou tout autre appareil par un site web, contenant les détails de votre historique de navigation sur ce site web parmi ses nombreuses utilisations.</p> <p>.
+</li>
+<li>
+<p><strong>Pays</strong> fait référence à :  France</p>
+</li>
+<li>
+<p><strong>Appareil</strong> désigne tout appareil pouvant accéder au Service tel qu'un ordinateur, un téléphone portable ou une tablette numérique</p> <p>.
+</li>
+<li>
+<p><strong>Données personnelles</strong> désigne toute information qui se rapporte à une personne physique identifiée ou identifiable.</p>
+</li>
+<li>
+</p><strong>Service</strong> fait référence au site web.</p> <p>.
+</li>
+<li>
+     <p><strong>Fournisseur de services</strong> désigne toute personne physique ou morale qui traite les données pour le compte de la Société. Il s'agit de sociétés tierces ou de personnes employées par la Société pour faciliter le Service, pour fournir le Service au nom de la Société, pour exécuter des services liés au Service ou pour aider la Société à analyser la façon dont le Service est utilisé.</p> <p>.
+</li>
+<li>
+<p><strong>Service de médias sociaux tiers</strong> désigne tout site web ou tout site web de réseau social par le biais duquel un utilisateur peut se connecter ou créer un compte pour utiliser le Service.</p>
+</li>
+<li>
+</p><strong>Données d'utilisation</strong> désigne les données collectées automatiquement, soit générées par l'utilisation du Service, soit provenant de l'infrastructure du Service elle-même (par exemple, la durée de visite d'une page).</p> <p>.
+</li>
+<li>
+<p><strong>Site internet</strong> désigne le croq'artCafé, accessible à partir de <a href="https://www.croq%27artCaf%C3%A9.fr" rel="external nofollow noopener" target="_blank">https://www.croq'artCafé.fr</a></p>.
+</li>
+<li>
+<p><strong>Vous</strong> désigne la personne physique qui accède ou utilise le service, ou la société, ou toute autre entité juridique au nom de laquelle cette personne physique accède ou utilise le service, le cas échéant.</p>
+</li>
+</ul>
+<h1>Collecte et utilisation de vos données personnelles</h1>
+<h2>Types de données collectées</h2>
+<h3>Données personnelles</h3>
+<p>Lorsque vous utilisez notre service, nous pouvons vous demander de nous fournir certaines informations personnellement identifiables qui peuvent être utilisées pour vous contacter ou vous identifier. Les informations personnellement identifiables peuvent inclure, mais ne sont pas limitées à :</p>
+<ul>
+<li>
+<p>Adresse électronique</p>
+</li>
+<li>
+<p>Prénom et nom de famille</p>
+</li>
+<li>
+<p>Numéro de téléphone</p>
+</li>
+<li>
+<p>Données d'utilisation</p>
+</li>
+</ul>
+<h3>Données d'utilisation</h3>
+<p>Les Données d'utilisation sont collectées automatiquement lors de l'utilisation du Service.</p> <p>.
+</p> <p>Les Données d'utilisation peuvent inclure des informations telles que l'adresse de protocole Internet (par exemple, l'adresse IP) de votre Appareil, le type de navigateur, la version du navigateur, les pages de notre Service que vous visitez, l'heure et la date de votre visite, le temps passé sur ces pages, les identifiants uniques de l'appareil et d'autres données de diagnostic.</p> <p>.
+</p> <p>Lorsque vous accédez au Service par ou via un appareil mobile, nous pouvons collecter certaines informations automatiquement, y compris, mais sans s'y limiter, le type d'appareil mobile que vous utilisez, l'identifiant unique de votre appareil mobile, l'adresse IP de votre appareil mobile, votre système d'exploitation mobile, le type de navigateur Internet mobile que vous utilisez, les identifiants uniques de l'appareil et d'autres données de diagnostic.</p> <p>.
+</p> <p>Nous pouvons également collecter les informations que votre navigateur envoie chaque fois que vous visitez notre service ou lorsque vous accédez au service par ou via un appareil mobile.</p>
+<h3>Technologies de suivi et cookies</h3>
+<p>Nous utilisons des Cookies et des technologies de suivi similaires pour suivre l'activité sur notre Service et stocker certaines informations. Les technologies de suivi utilisées sont des balises, des tags et des scripts pour collecter et suivre des informations et pour améliorer et analyser Notre Service. Les technologies que Nous utilisons peuvent inclure :</p>
+<ul>
+<li><strong>Cookies ou cookies de navigateur.</strong> Un cookie est un petit fichier placé sur Votre appareil. Vous pouvez demander à Votre navigateur de refuser tous les Cookies ou d'indiquer quand un Cookie est envoyé. Cependant, si Vous n'acceptez pas les Cookies, Vous ne pourrez peut-être pas utiliser certaines parties de notre Service. À moins que vous n'ayez réglé les paramètres de votre navigateur pour qu'il refuse les Cookies, notre Service peut utiliser des Cookies.</li>
+<li><strong>Cookies Flash.</strong> Certaines fonctionnalités de notre Service peuvent utiliser des objets stockés localement (ou Cookies Flash) pour collecter et stocker des informations sur vos préférences ou votre activité sur notre Service. Les Cookies Flash ne sont pas gérés par les mêmes paramètres de navigateur que ceux utilisés pour les Cookies de navigateur. Pour plus d'informations sur la manière dont vous pouvez supprimer les cookies Flash, veuillez lire &quot;Où puis-je modifier les paramètres de désactivation ou de suppression des objets locaux partagés?&quot ; disponible sur <a href="https://helpx.adobe.com/flash-player/kb/disable-local-shared-objects-flash.html#main_Where_can_I_change_the_settings_for_disabling__or_deleting_local_shared_objects_" rel="external nofollow noopener" target="_blank">https://helpx.adobe.com/flash-player/kb/disable-local-shared-objects-flash.html#main_Where_can_I_change_the_settings_for_disabling__or_deleting_local_share.
+d_objets_</a></li>.
+<li><strong>Balises web.</strong> Certaines sections de notre Service et nos courriels peuvent contenir de petits fichiers électroniques connus sous le nom de balises web (également appelés gifs transparents, balises pixel et gifs à pixel unique) qui permettent à la Société, par exemple, de compter les utilisateurs qui ont visité ces pages ou ouvert un courriel et pour d'autres statistiques de site web connexes (par exemple, enregistrer la popularité d'une certaine section et vérifier l'intégrité du système et du serveur).</li>
+</ul>
+</p> <p>Les cookies peuvent être des cookies &quot;persistants&quot ; ou des cookies &quot;de session&quot ;. Les Cookies persistants restent sur votre ordinateur personnel ou votre appareil mobile lorsque vous vous déconnectez, tandis que les Cookies de session sont supprimés dès que vous fermez votre navigateur web. En savoir plus sur les cookies : <a href="https://www.privacypolicies.com/blog/cookies/" target="_blank">Qu'est-ce que les cookies ? </a>.</p>
+</p> Nous utilisons à la fois des cookies de session et des cookies persistants aux fins énoncées ci-dessous :</p>
+<ul>
+<li>
+<p><strong>Cookies nécessaires / essentiels</strong></p>.
+<p>Type : Cookies de session</p>
+<p>Administrés par : Nous</p>
+</p> <p>Objectif : Ces Cookies sont essentiels pour Vous fournir les services disponibles sur le Site Internet et pour Vous permettre d'utiliser certaines de ses fonctionnalités. Ils contribuent à authentifier les utilisateurs et à empêcher l'utilisation frauduleuse des comptes d'utilisateurs. Sans ces Cookies, les services que Vous avez demandés ne peuvent pas être fournis, et Nous utilisons ces Cookies uniquement pour Vous fournir ces services.</p> <p>.
+</li>
+<li>
+<p><strong>Politique des cookies / Cookies d'acceptation de l'avis</strong></p><p>.
+<p>Type : Cookies persistants</p> <p>.
+<p>Administrés par : Nous</p>
+</p> <p>Objectif : Ces cookies identifient si les utilisateurs ont accepté l'utilisation de cookies sur le site Web.</p> <p>.
+</li>
+<li>
+<p><strong>Cookies de fonctionnalité</strong></p> <p>.
+<p>Type : Cookies persistants</p> <p>.
+<p>Administrés par : Nous</p> <p>
+</p> <p>Objectif : Ces Cookies nous permettent de mémoriser les choix que Vous faites lorsque Vous utilisez le Site Internet, comme la mémorisation de vos données de connexion ou de votre préférence linguistique. L'objectif de ces Cookies est de Vous fournir une expérience plus personnelle et de Vous éviter de devoir saisir à nouveau vos préférences chaque fois que Vous utilisez le Site Internet.</p> <p>.
+</li>
+</ul>
+</p> <p>Pour plus d'informations sur les cookies que nous utilisons et sur vos choix en matière de cookies, veuillez consulter notre Politique en matière de cookies ou la section Cookies de notre Politique de confidentialité.</p> <p>.
+<h2>Utilisation de vos données personnelles</h2>
+<p>La société peut utiliser les données personnelles aux fins suivantes :</p>
+<ul>
+<li>
+<p><strong>Pour fournir et maintenir notre Service</strong>, y compris pour surveiller l'utilisation de notre Service.</p>
+</li>
+<li>
+<p><strong>Pour gérer votre compte :</strong> pour gérer votre inscription en tant qu'utilisateur du Service. Les Données personnelles que vous fournissez peuvent vous donner accès à différentes fonctionnalités du Service qui vous sont accessibles en tant qu'utilisateur enregistré.</p> <p>.
+</li>
+<li>
+<p><strong>Pour l'exécution d'un contrat :</strong> l'élaboration, le respect et l'engagement du contrat d'achat des produits, articles ou services que Vous avez achetés ou de tout autre contrat avec Nous par l'intermédiaire du Service.</p>
+</li>
+<li>
+</p><strong>Vous contacter :</strong> Vous contacter par courrier électronique, appels téléphoniques, SMS, ou autres formes équivalentes de communication électronique, telles que les notifications push d'une application mobile concernant les mises à jour ou les communications informatives liées aux fonctionnalités, produits ou services contractés, y compris les mises à jour de sécurité, lorsque cela est nécessaire ou raisonnable pour leur mise en œuvre.</p> <p>
+</li>
+<li>
+<p><strong>Pour vous fournir</strong> des nouvelles, des offres spéciales et des informations générales sur d'autres biens, services et événements que nous proposons et qui sont similaires à ceux que vous avez déjà achetés ou sur lesquels vous vous êtes renseigné, à moins que vous n'ayez choisi de ne pas recevoir de telles informations.</p>
+</li>
+<li>
+<p><strong>Gérer vos demandes :</strong> Assister et gérer les demandes que vous nous adressez.</p>
+</li>
+<li>
+</p><strong>Pour les transferts d'entreprise:</strong>Nous pouvons utiliser Vos informations pour évaluer ou réaliser une fusion, une cession, une restructuration, une réorganisation, une dissolution ou toute autre vente ou transfert de tout ou partie de Nos actifs, que ce soit dans le cadre d'une poursuite d'activité ou d'une faillite, d'une liquidation ou d'une procédure similaire, dans laquelle les Données personnelles que Nous détenons sur les utilisateurs de nos Services font partie des actifs transférés.</p> <p>
+</li>
+<li>
+<p><strong>À d'autres fins</strong> : Nous pouvons utiliser Vos informations à d'autres fins, telles que l'analyse des données, l'identification des tendances d'utilisation, la détermination de l'efficacité de nos campagnes promotionnelles et l'évaluation et l'amélioration de notre Service, de nos produits, de nos services, de notre marketing et de votre expérience.</p> <p>.
+</li>
+</ul>
+</p> <p>Nous pouvons partager vos informations personnelles dans les situations suivantes :</p> <p>.
+<ul>
+<li><strong>Avec les fournisseurs de services :</strong> Nous pouvons partager vos informations personnelles avec les fournisseurs de services pour surveiller et analyser l'utilisation de notre service, pour vous contacter.</li>
+
+
+
+
+
+
+</div>
+            
+
+
+<?php
+get_footer();
