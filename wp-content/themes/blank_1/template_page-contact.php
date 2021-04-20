72205422cfc0001b753446151613bfb119a691ef
heros20 <heros40@hotmail.fr>
2021-04-20 17:47:41 +0200

    responsiv

--- wp-content/themes/blank_1/template_page-contact.php
+++ wp-content/themes/blank_1/template_page-contact.php
@@ -137,6 +137,7 @@ if ($success == true) { ?>
                         <p> <?= get_the_content() ?> </p>
                     </div>
                 </li>
+                <div class="trait"></div>
             <?php  } ?>
         <?php } ?>
     </ul>
