96dee262a5b8b0ed7c1d1910e38e5d7529a2749b
ZenonStella <stellazenon@gmail.com>
2021-04-23 10:55:15 +0200

    mail

--- wp-content/themes/blank_1/inc/admin/admin-moderation.php
+++ wp-content/themes/blank_1/inc/admin/admin-moderation.php
@@ -19,8 +19,12 @@ function my_custom_menu_page_moderation(){
         $id = $_GET['id'];
         global $wpdb;
         $table = $wpdb->prefix.'reservation';
+        $table2 = $wpdb->prefix.'client';
         $sdl =  "SELECT * FROM $table WHERE id = $id ";
         $reservations = $wpdb->get_results($sdl, ARRAY_A);
+        $id_client = $reservations[0]['id_client'];
+        $sql =  "SELECT * FROM $table2 WHERE id = $id_client ";
+        $clients = $wpdb->get_results($sql, ARRAY_A);
         $adminUrl = admin_url().'admin.php?page=custompage_moderation&id='.$id;
         echo $id;
         $errors = array();
@@ -42,19 +46,30 @@ function my_custom_menu_page_moderation(){
                         <p>La modification à bien été prise en compte</p>
                         <p>La réservation à bien été accepée</p>
                         <?php 
+                        $message = "Bonjour Monsuieur ".$clients[0]['nom'].",\r\nVotre réservation a bien été accèpter\r\nMerci de votre confiance.\r\nLe Croq'Art Café";
+                        mail($clients[0]['email'], 'Réservation', $message);
                         wp_safe_redirect('admin.php?page=custompage_reservation');
                     } 
                 }
                 else {
-                    $wpdb->delete( $table, array( 'id' => $id ) );?>
+                    $wpdb->delete( 
+                        $table, 
+                        array( 'id' => $id ),
+                        $table2,
+                        array( 'id' => $reservations[0]['id_cient'])
+                    );?>
                     <p>La réservation à bien été supprimer</p>
-                <?php }
+                    <?php 
+                    $message = "Bonjour Monsuieur ".$clients[0]['nom'].",\r\nVotre réservation n'a, maleureusement, pas été accèptée, certainement dû a un manque de place\r\nNos plus sinsères excuses,\r\nLe Croq'Art Café";
+                    mail($clients[0]['email'], 'Réservation', $message);
+                }
                 $success = true;
             }
             debug($_POST);
         }
+        // echo $reservations[0]['id_client'];
         debug($reservations);
-        // debug($);
+        debug($clients);
     }
 ?>
 <form action="" method="POST" novalidate>
