361067b19e3c1447fdd052775b729afe4f0332a8
heros20 <heros40@hotmail.fr>
2021-04-25 16:15:00 +0200

    oage 404

--- wp-content/themes/blank_1/functions.php
+++ wp-content/themes/blank_1/functions.php
@@ -18,7 +18,6 @@ require get_template_directory() . '/inc/custom/custom-menu.php';
 require get_template_directory() . '/inc/custom/custom-contact.php';
 require get_template_directory() . '/inc/custom/custom-footer.php';
 require get_template_directory() . '/inc/custom/custom-gallery.php';
-remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 
 
 
