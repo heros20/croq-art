95ffb8a3f8dd8edab0d9d320790cd7d0d232e8bc
heros20 <heros40@hotmail.fr>
2021-04-26 10:01:46 +0200

    404

--- wp-content/themes/blank_1/404.php
+++ wp-content/themes/blank_1/404.php
@@ -9,8 +9,11 @@
 
 get_header();
 ?>
+<div class="b errorImg">
+ <img src="<?= get_template_directory_uri(); ?>/assets/images/404.jpg" alt="">
+</div>
+
 <div class="b errorPage">
- <p><img src="<?= get_template_directory_uri(); ?>/assets/images/404.jpg" alt=""></p>
  <p>Aïe ! Cette page est introuvable.</p>
  <p>Il est possible que cette page ait été déplacer ou supprimer</p>
  <a href="<?= esc_url(home_url('/')) ?>">Nous vous invitons à retourner sur la page d'accueil</a>
