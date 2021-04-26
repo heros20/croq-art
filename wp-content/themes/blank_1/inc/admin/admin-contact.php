8e53e2082b500b1dbbaa495ff59a893542db67cf
heros20 <heros40@hotmail.fr>
2021-04-26 12:25:52 +0200

    admin contact terminer

--- wp-content/themes/blank_1/inc/admin/admin-contact.php
+++ wp-content/themes/blank_1/inc/admin/admin-contact.php
@@ -50,7 +50,7 @@ function my_custom_menu_page_contact(){
                         <td><?= $contact['numero'] ?></td>
                         <td><?= $contact['message'] ?></td>
                         <td><?= date('d/m/Y à H:i',strtotime($contact['created_at'])) ?></td>
-                        <td><a href="admin.php?page=custompage_delete&id=<?= $contact['id'] ?>">Supprimer</a></td>
+                        <td><a href="admin.php?page=custompage_contact&id=<?= $contact['id'] ?>">Supprimer</a></td>
                     </tr>
                     <?php } ?>
             </table>
@@ -58,5 +58,15 @@ function my_custom_menu_page_contact(){
         
     </div>
    
-<?php } ?>
+    <?php } 
+    if(!empty($_GET['id'])){
+            $id = $_GET['id'];
+            global $wpdb;
+            $table = $wpdb->prefix.'message';
+            $sdl =  "SELECT * FROM $table WHERE id = $id ";
+            $message = $wpdb->get_results($sdl, ARRAY_A);
+
+            $wpdb->delete( $table, array( 'id' => $id ) );
+        }
+    ?>
 
