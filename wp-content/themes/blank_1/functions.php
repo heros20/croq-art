9293bce2d85bcfb1efdcfb1d2a2492bfe8de69f3
heros20 <heros40@hotmail.fr>
2021-04-28 13:25:53 +0200

    page detail terminer

--- wp-content/themes/blank_1/functions.php
+++ wp-content/themes/blank_1/functions.php
@@ -18,9 +18,24 @@ require get_template_directory() . '/inc/custom/custom-menu.php';
 require get_template_directory() . '/inc/custom/custom-contact.php';
 require get_template_directory() . '/inc/custom/custom-footer.php';
 require get_template_directory() . '/inc/custom/custom-gallery.php';
+
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
    add_theme_support( 'woocommerce' );
 } 
+function woocommercefunction() {
+	 if (is_product_category()){
+      echo '<a class="button continue" href="https://bay-key.fr/shop/" >Retour à la boutique</a>';
+      echo '<button class="button continue" style="display:block;" onclick="history.go(-1);">Page précédente</button>';
+	}
+ }
+add_action( 'woocommerce_before_shop_loop', 'woocommercefunction');
+
+// function bbloomer_continue_shopping_button() {
+//    echo '<a class="button continue" href="https://bay-key.fr/shop/">Retour à la boutique</a>';
+//    echo '<button style="display:block;" onclick="history.go(-3);">Page précédente</button>';
+// } 
+// add_action( 'woocommerce_after_add_to_cart_button', 'bbloomer_continue_shopping_button', 50 );
+
 
 
