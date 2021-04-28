daba4b1f9c59045a2bf145a2627ccde0d1dfcc05
ZenonStella <stellazenon@gmail.com>
2021-04-28 12:21:58 +0200

    petites modifs

--- wp-content/themes/blank_1/inc/general.php
+++ wp-content/themes/blank_1/inc/general.php
@@ -52,7 +52,7 @@ function blank_1_scripts() {
 	if (is_page(94)) {
 		wp_enqueue_style('avis-style', get_template_directory_uri() . '/assets/css/avis.css', array(), _S_VERSION);
 	}
-	if (is_page(96)) {
+	if (is_page(25)) {
 		wp_enqueue_style('menu-style', get_template_directory_uri() . '/assets/css/menu.css', array(), _S_VERSION);
 	}
 	if (is_page(98)) {
