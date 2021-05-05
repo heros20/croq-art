87551904d3196ae1ff247df685603ef7ac15f676
heros20 <heros40@hotmail.fr>
2021-05-05 10:03:46 +0200

    for pull

--- wp-content/themes/blank_1/inc/general.php
+++ wp-content/themes/blank_1/inc/general.php
@@ -33,32 +33,32 @@ add_action( 'after_setup_theme', 'blank_1_content_width', 0 );
 function blank_1_scripts() {
 	wp_enqueue_style( 'blank_1-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	if(is_page(8)){
+	if(is_page(10)){
 		wp_enqueue_style( 'flexslider-style',get_template_directory_uri().'/assets/flexslider/flexslider.css', array(), _S_VERSION);
 		wp_enqueue_style('home-style', get_template_directory_uri() . '/assets/css/home.css', array(), _S_VERSION);
 		wp_enqueue_script( 'jquery-js','https://code.jquery.com/jquery-3.6.0.min.js', array(), _S_VERSION, true );
 		wp_enqueue_script( 'flexslider-js',get_template_directory_uri().'/assets/flexslider/jquery.flexslider-min.js', array(), _S_VERSION, true );
 		wp_enqueue_script( 'main-js',get_template_directory_uri().'/assets/js/main.js', array(), _S_VERSION, true );
 	}
-	if (is_page(9)) {
+	if (is_page(13)) {
 		wp_enqueue_style('clickandcollect-style', get_template_directory_uri() . '/assets/css/clickandcollect.css', array(), _S_VERSION);
 	}
-	if (is_page(9)) {
+	if (is_page(15)) {
 		wp_enqueue_style('reservation-style', get_template_directory_uri() . '/assets/css/reservation.css', array(), _S_VERSION);
 	}
-	if (is_page(16)) {
+	if (is_page(91)) {
 		wp_enqueue_style('contact-style', get_template_directory_uri() . '/assets/css/contact.css', array(), _S_VERSION);
 	}
-	if (is_page(20)) {
+	if (is_page(94)) {
 		wp_enqueue_style('avis-style', get_template_directory_uri() . '/assets/css/avis.css', array(), _S_VERSION);
 	}
-	if (is_page(25)) {
+	if (is_page(96)) {
 		wp_enqueue_style('menu-style', get_template_directory_uri() . '/assets/css/menu.css', array(), _S_VERSION);
 	}
-	if (is_page(10)) {
+	if (is_page(98)) {
 		wp_enqueue_style('presentation-style', get_template_directory_uri() . '/assets/css/presentation.css', array(), _S_VERSION);
 	}
-	if (is_page(18)) {
+	if (is_page(101)) {
 		wp_enqueue_style('gallery-style', get_template_directory_uri() . '/assets/css/gallery.css', array(), _S_VERSION);
 	}
 }
