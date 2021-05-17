acffa6b4700d274a868e579c74df5c7e89a113ac
heros20 <heros40@hotmail.fr>
2021-05-17 12:56:37 +0200

    salopard

--- wp-content/themes/blank_1/assets/js/reservation.js
+++
@@ -1,9 +0,0 @@
-var otherCheckbox = document.querySelector('input[value="accept"]');
-
-otherCheckbox.onchange = function() {
-  if(otherCheckbox.checked == false) {
-    document.getElementById('checkError').innerHTML = "je change le text";
-  } else {
-    otherText.style.visibility = 'hidden';
-  }
-};
